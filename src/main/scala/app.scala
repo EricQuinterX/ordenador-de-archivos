e7f385acf872c8c589d85c1d196b1f81d3f08a46
Eric Brian Quinteros <Eric Brian Quinteros>
2017-03-07 01:31:49 -0300

    creo el proceso de configuracion

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -1,10 +1,10 @@
 package app_procesador
 
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{ConfigFactory, Config}
 import scala.swing._
 import scala.swing.event._
 import Swing.{HStrut, VStrut, EmptyBorder}
-import scala.util.Try
+import scala.util.{Try, Success, Failure}
 import java.io.File
 import java.awt.Font
 import javax.swing.BorderFactory
@@ -16,30 +16,6 @@ object app {
     ui.pack()
     ui.visible = true
   }
-}
-
-case class ConfigFile() {
-	def getVersion : String = {
-		Try(ConfigFactory.load("application.config").getString("foldername")) match {
-			case Success(s) => s
-			case Failure(_) => throw new ErrorCargarConfig("Hubo error al cargar la version de la configuracion")
-		}
-	}
-	def getFoldername : String = {
-		val name_folder = Try(ConfigFactory.load("application.config").getString("foldername")) 
-		name_folder match {
-			case Success(s) = s
-			case Failure(_) = throw new ErrorCargarConfig("Error cargar nombre de la carpeta del config")
-		}
-	}
-	def getExecOrderList : List[String] = {
-		import scala.collection.JavaConversions._
-		val lista_orden = Try (ConfigFactory.load("application.config").getStringList("execOrder").toList)
-		lista_orden match {
-			case Success(xs) = xs
-			case Failure(_) = throw new ErrorCargarConfig("Error cargar orden de ejecucion del config")	
-		}
-	}
 }
 
 class UI extends MainFrame {
@@ -106,31 +82,49 @@ class UI extends MainFrame {
 
 case class ErrorCargarConfig(msg: String, cause: Throwable = null) extends Exception(msg, cause)
 
-case class FileKeyword (word: String, folder: String)
+case class FileKeyword (params: Config) {
+	val keyword: String = params.getString("keyword")
+	val folder : String = params.getString("folder")
+}
+
+object ConfigFile {
+	val configFile = ConfigFactory.parseFile(new File("application.conf")).getConfig("app")
+	val config = ConfigFactory.load(configFile)
+	def getVersion : String = {
+		Try(config.getString("version")) match {
+			case Success(s) => s
+			case Failure(_) => throw new ErrorCargarConfig("Hubo error al cargar la version de la configuracion")
+		}
+	}
+	def getFoldername : String = {
+		val name_folder = Try(config.getString("folder")) 
+		name_folder match {
+			case Success(s) => s
+			case Failure(_) => throw new ErrorCargarConfig("Error cargar nombre de la carpeta del config")
+		}
+	}
+	def getExecOrderList : List[FileKeyword] = {
+		import scala.collection.JavaConversions._
+		val lista_orden = Try {
+			val lista = config.getConfigList("priority")
+			(lista.map(FileKeyword(_))).toList
+		}
+		lista_orden match {
+			case Success(xs) => xs
+			case Failure(_) => throw new ErrorCargarConfig("Error cargar orden de ejecucion del config")	
+		}
+	}
+}
 
 case class Core (gui: UI) {
 
-	var filesystem 	 : FileSystemFilter = _
-
-	/*
-	val priority: Map[Int, FileKeyword] = Map (
-		0 -> FileKeyword(".Deletes.","OLD"),
-		1 -> FileKeyword(".Deletes.","NEW"),
-		2 -> FileKeyword(".Inserts.","NEW"),
-		3 -> FileKeyword("CREATE_TABLE","NEW"),
-		4 -> FileKeyword("CREATE_INDEX","NEW"),
-		5 -> FileKeyword("ALTER_TABLE","NEW"),
-		6 -> FileKeyword("ALTER_INDEX","NEW"),
-		7 -> FileKeyword("FUNCITON","NEW"),
-		8 -> FileKeyword("PROCEDURE","NEW"),
-		9 -> FileKeyword("PACKAGE","NEW")
-	)*/
+	var filesystem  : FileSystemFilter = _
+	var folderResult: String = _
 
 	def procesar() = {
-		Try {
+		val resultado = Try {
 			// cada vez que haga clic en el boton se debe limpiar el Detalle
-			folderResult = getFoldername()
-			execOrderList = getExecOrderList()
+			folderResult = ConfigFile.getFoldername
 			gui.txtAreaOutput.text = ""
 			blockComponents()
 			val new_files = getListOfFiles(path + "\\NEW")
@@ -139,16 +133,27 @@ case class Core (gui: UI) {
 			var i = 0
 			val configPriority = ConfigFile.getExecOrderList
 			
-			filesystem = FileSystemFilter(all_files, gui.txtAreaOutput)
+			filesystem = FileSystemFilter(all_files)
 
-			for (i <- 0 until priority.size)
-				filesystem = filesystem.start( priority(i).word, priority(i).folder )
+			for (i <- 0 until configPriority.size)
+				filesystem = filesystem.start( configPriority(i).keyword, configPriority(i).folder )
 			createAndCopyFiles()
 			gui.txtAreaOutput.append(s"${filesystem.index.toString} fueron acomodados y enumerados \n")
-		} getOrElse {
-			gui.txtAreaOutput.append("Hubo un Error" )
-			unBlockComponents()
 		}
+		resultado match {
+			case Success(_) => unBlockComponents()
+			case Failure(_) => 
+				gui.txtAreaOutput.append("Hubo un Error al procesar el pasaje" )
+				unBlockComponents()
+		}
+	}
+
+	private def getListOfFiles(dir: String): List[File] = {
+    val d = new File(dir)
+    if (d.exists && d.isDirectory)
+      d.listFiles.filter(_.isFile).toList
+    else
+      List[File]()
 	}
 
 	private def path : String = gui.txtInputPath.text
@@ -179,21 +184,12 @@ case class Core (gui: UI) {
 	}
 
 	private def resolvePath(path: String): String = path.replaceAllLiterally("\\","\\\\")
-
-	private def getListOfFiles(dir: String): List[File] = {
-    val d = new File(dir)
-    if (d.exists && d.isDirectory)
-      d.listFiles.filter(_.isFile).toList
-    else
-      List[File]()
-	}
 }
 
 
 case class FileSystemFilter(remainingFiles: List[File], 
-														componente: TextArea, 
-														index: Int = 0, 
-														filemap: List[(File, String)] = Nil) {
+							index: Int = 0, 
+							filemap: List[(File, String)] = Nil) {
 
 	def start (word: String, foldername: String): FileSystemFilter = {
 		var lista: List[(File, String)] = Nil
@@ -203,7 +199,6 @@ case class FileSystemFilter(remainingFiles: List[File],
 			if file.getName.contains(word)
 		} yield {
 			val p = genNewNameFile(i, foldername, file.getName )
-			componente.append(p + "\n")
 			lista = lista :+ ((file, p ))
 			i += 1
 		}
@@ -211,7 +206,10 @@ case class FileSystemFilter(remainingFiles: List[File],
 		copy(index = i, filemap = filemap ::: lista, remainingFiles = resto)
 	}
 
-	private def genNewNameFile(i: Int, prefix: String, filename: String) = genStringIndex(i) + "_" + prefix + "_" + filename
+	private def genNewNameFile(i: Int, prefix: String, filename: String) = prefix match {
+		case "OLD" => genStringIndex(i) + "_" + prefix + "_" + filename
+		case "NEW" => genStringIndex(i) + "_" + filename
+	}
 
 	private def genStringIndex(i: Int) = String.valueOf(i).length() match {
 		case 1 => "00" + i.toString()
@@ -219,3 +217,10 @@ case class FileSystemFilter(remainingFiles: List[File],
 		case _ => i.toString()
 	}
 }
+/*
+case class ValidationProcess (amountProcessed: Int) {
+	val countOldFiles = getListOfFiles(path + "\\OLD").size
+	val countNewFiles = getListOfFiles(path + "\\NEW").size
+	val total = countNewFiles + countOldFiles
+	amountProcessed == total
+}*/
