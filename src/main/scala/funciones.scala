dfae7fdb51a1416fc4d340e0ef3691a11e05cc80
EricQuinterX <eric.brian.quinteros@gmail.com>
2017-03-19 13:31:59 -0300

    Indice de los archivos generados automaticos y capacidad de analizar n carpetas"

--- src/main/scala/funciones.scala
+++ src/main/scala/funciones.scala
@@ -35,24 +35,28 @@ trait Modulo {
 }
 
 
+
 class Ordenador (ruta : String, out : TextArea) extends Modulo {
 
   var index: Int = 0
   var restantes : List[File] = Nil
   var ordenados : List[(File, String)] = Nil
+  var total : Int = 0
+  val folders : List[Folder] = ConfigFile.getFolderList
   val carpeta_destino : String = ConfigFile.getFolder()
 
   def startFunction = {
     // Cada vez que ejecuta esta funcionalidad, recupero la lista de filtros si es que lo modifico en tiempo de ejecucion
     val filtros : List[FileKeyword] = ConfigFile.getFilters
-    val `tamFiltros` = filtros.size
+    val tamFiltros = filtros.size
     // La secuencia empieza siempre en 000
     var i : Int = 0
-    // Meto los archivos de las 2 carpeta en una lista
-    restantes = getFiles.sorted
+    // Meto los archivos de las carpetas en una lista
+    restantes = getFiles(ruta).sorted
+    total = restantes.size
     // Recorro la lista de filtros de arriba hacia abajo
-    for (i <- 0 to `tamFiltros`) {
-      if (i == `tamFiltros`)
+    for (i <- 0 to tamFiltros) {
+      if (i == tamFiltros)
         // llego al ultimo, ejecuto los restantes
         filtrar(".", "", ".*")
       else
@@ -61,24 +65,25 @@ class Ordenador (ruta : String, out : TextArea) extends Modulo {
     }
     // Copio los archivos de las 2 carpetas a un nueva en base a los nuevos nombres de la lista nueva
     createFiles(new File(ruta + "\\" + carpeta_destino))
-    out.append(index + " archivos fueron organizados\n")
+    out.append(s"($index/$total) archivos fueron organizados\n")
+    // Verifico si se organizaron todos los archivos de las carpetas
+    val finalMsg = if (total == index) "La operacion se completo satisfactoriamente" else "No se organizaron todos los archivos"
+    out.append(s"$finalMsg\n")
   }
 
-  private def getFiles(r: String): List[File] = ConfigFile.getFolderList.flatMap(getFilesFrom(r + "\\" + _.name))
+
+  private def getFiles(r: String): List[File] = ConfigFile.getFolderList.flatMap(folder => getFilesFrom(r + "\\" + folder.name))
+
 
 	private def filtrar (word: String, folder: String, pos: String): Unit = {
     // Funcion que compara el nombre de la carpeta "NEW/OLD", el patron "keyword" y la posicion "INICIO/MEDIO"
     def criteria(x: File) : Boolean = {
-      val sameFolder = x.getParentFile.getName == folder
       val regex = word.replaceAllLiterally("***", ".*") + ".*"
       val patron = Pattern.compile(regex)
       val matcher = patron.matcher(x.getName)
       if (matcher.find) {
-        val position = matcher.start match {
-          case 0 => "INICIO"
-          case n => "MEDIO"
-        }
-        sameFolder && position == pos
+        val p = if (matcher.start == 0) "INICIO" else "MEDIO"
+        x.getParentFile.getName == folder && p == pos
       } else false
     }
     // La lista de los archivos filtrados para luego sacar una diferencia
@@ -93,30 +98,32 @@ class Ordenador (ruta : String, out : TextArea) extends Modulo {
     restantes = restantes filterNot filtrados.contains
 	}
 
+
   private def createFiles(dir: File) = {
     Try(clearAndCreateDir(dir)) match {
-      case Success(s) => if (!s) throw new ErrorCrearCarpeta("No se limpio la carpeta ARCHIVOS_ORGANIZADOS")
+      case Success(s) => if (!s) throw new ErrorCrearCarpeta("No se limpio la carpeta destino")
       case Failure(e) => out.append(e.getMessage)
     }
+    out.append(s"Directorio ${dir.getName} creado\n")
+    out.append(s"Archivos creados:\n")
     // CREO LOS ARCHIVOS SQL CON SUS RESPECTIVOS NOMBRES
     for ((src, name) <- ordenados.sortBy(_._2)){
       val destin = new File(dir.getPath + "\\" + name)
       val fos = new FileOutputStream(destin)
       fos.getChannel.transferFrom(new FileInputStream(src).getChannel, 0, Long.MaxValue)
       fos.close // IMPORTANTE CERRAR EL ARCHIVO
-      out.append("Archivo creado: " + name + "\n")
+      out.append("  " + name + "\n")
     }
   }
 
-	private def getNewName (i: Int, prefix: String, filename: String) = prefix match {
-		case "OLD" => getIndex(i) + "_" + prefix + "_" + filename
-		case "NEW" => getIndex(i) + "_" + filename // a los archivos del /NEW no muestro este prefijo
-	}
+	private def getNewName (i: Int, prefix: String, filename: String) = {
+    if (folders.filter(_.name == prefix).map(_.show).head)
+      getIndex(i,total) + "_" + prefix + "_" + filename
+    else
+      getIndex(i,total) + "_" + filename
+  }
 
   // Creo el indice de los archivos
-	private def getIndex(i: Int) = i.toString.length match {
-		case 1 => "00" + i.toString
-		case 2 => "0" + i.toString
-		case _ => i.toString
-	}
+	private def getIndex(i: Int, max: Int) = "0" * (max.toString.length - i.toString.length) + i.toString
+
 }
